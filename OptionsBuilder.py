e618b34508f37c38ada005b0c3432d5c90baf514
senlac <hcalderhm@gmail.com>
2022-05-24 14:12:37 +0100

    Fixed a file name bug

--- OptionsBuilder.py
+++ OptionsBuilder.py
@@ -96,4 +96,5 @@ for item in newItems:
     with open(filePath,'w') as f:
         writer = csv.writer(f, lineterminator='\n')
         writer.writerows(item)
+        
 tk.messagebox.showinfo(title="Job Complete", message="Selected Folder has been populated")
